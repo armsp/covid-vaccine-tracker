b04374a09dc74b14dfc6dc9c86d19a692871b269
armsp <shantamdps@gmail.com>
2021-02-20 15:55:38 +0530

    removing all history, starting fresh

---
+++ chart_util.py
@@ -0,0 +1,79 @@
+import altair as alt
+import pandas as pd
+import geopandas as gpd
+from functools import lru_cache
+
+alt.renderers.set_embed_options(actions=False)
+
+chart_template = """
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://cdn.jsdelivr.net/npm/vega@{vega_version}"></script>
+  <script src="https://cdn.jsdelivr.net/npm/vega-lite@{vegalite_version}"></script>
+  <script src="https://cdn.jsdelivr.net/npm/vega-embed@{vegaembed_version}"></script>
+  <script src="https://cdn.jsdelivr.net/npm/vega-projection-extended@2"></script>
+</head>
+<body>
+
+<div id="vis"></div>
+
+<script type="text/javascript">
+  vegaEmbed('#vis', {spec}, {kwargs}).catch(console.error);
+</script>
+</body>
+</html>
+"""
+
+@lru_cache
+def get_shapefiles():
+    uri_50m = "https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip"
+    uri_110m = "https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries.zip"
+    countries_raw = gpd.read_file(uri_50m)
+
+    countries_map = countries_raw.drop(columns=list(countries_raw.columns[:17]) + list(countries_raw.columns[18:46]) + list(countries_raw.columns[47:-1]))
+    countries_map = countries_map.rename(columns={'ISO_A3': 'iso_code'})
+    return countries_map
+
+def make_nyt_chart():
+    pass
+
+def make_owd_chart():
+    vaccine_location_data = pd.read_csv('./owd_datasets/vaccine_approval_owd.csv')
+    vaccine_location_data['vaccines'] = vaccine_location_data.vaccines.apply(lambda x: list(map(str.lstrip, x.split(','))))
+    vaccine_location_data = vaccine_location_data.explode('vaccines').reset_index(drop=True)
+
+    countries_map = get_shapefiles()
+    countries_map.loc[countries_map.NAME == 'France', 'iso_code'] = "FRA"
+    countries_map.loc[countries_map.NAME == 'Norway', 'iso_code'] = "NOR"
+    vaccine_location_data.loc[vaccine_location_data.location.isin(['England', 'Wales', 'Scotland', 'Northern Ireland']), 'iso_code'] = "GBR"
+
+    plot_data = countries_map.merge(vaccine_location_data, how='inner', on='iso_code')
+    base = alt.Chart(countries_map[countries_map.iso_code!='ATA']).mark_geoshape(fill='#eee', stroke="#fff", strokeWidth=0.5)
+
+    chart = alt.concat(*(
+                base + alt.Chart(plot_data[plot_data['vaccines']==vaccine], title=vaccine, height=200, width=350).mark_geoshape(stroke="#fff", strokeWidth=0.5).encode(
+                    color=alt.value('#2e7265')
+                ).project('equalEarth')
+                for vaccine in list(plot_data.vaccines.unique())
+                ), columns=3, title="Where each vaccine is being used", spacing=0
+            ).configure_view(strokeWidth=0)
+
+    return chart
+
+def modify_owd_chart(chart):
+    with open('index.html', 'w+') as f:
+        f.write(chart_template.format(
+            vega_version=alt.VEGA_VERSION,
+            vegalite_version=alt.VEGALITE_VERSION,
+            vegaembed_version=alt.VEGAEMBED_VERSION,
+            spec=chart.to_json(indent=None),
+            kwargs = """{"actions": false, "mode": "vega-lite"}"""
+        ))
+        f.seek(0)
+        filedata = f.read()
+        filedata = filedata.replace('equalEarth', 'eckert3')
+
+if __name__=='__main__':
+    chart = make_owd_chart()
+    modify_owd_chart(chart)
---
+++ nyt_util.py
@@ -0,0 +1,89 @@
+'''
+Utilities for handling and cleaning of the NYT dataset scraped from their Vaccine tracker website https://www.nytimes.com/interactive/2020/science/coronavirus-vaccine-tracker.html
+'''
+import pandas as pd
+import unicodedata
+from requests_html import HTMLSession
+
+from path_util import get_nyt_dataset_path
+
+NYT_URI = "https://www.nytimes.com/interactive/2020/science/coronavirus-vaccine-tracker.html"
+
+SELECTOR_APPROVED = "p.g-filter-approved > span.g-vacinfo"# > span.g-info"
+SELECTOR_P3 = ""
+SELECTOR_P2 = ""
+SELECTOR_P1 = ""
+SELECTOR_PRECLINICAL = ""
+SELECTOR_ABANDONED = ""
+SELECTOR_ALL = ""
+
+def get_NYT_webpage(uri):
+    session = HTMLSession()
+    r = session.get(uri)
+    return r
+
+def process_vaccine_info(all_info_dict):
+    return {unicodedata.normalize("NFKD", key.strip()): value.strip() for key, value in (info.split(':') if all_info_dict['vaccine_info'] != '' else ['None','None'] for info in all_info_dict['vaccine_info'].split('\n'))}
+
+def process_location_info(all_info_dict):
+    return {key.strip(): value.strip() for key, value in (info.split(':') for info in all_info_dict['location_info'].split('\n'))}
+
+def get_vaccine_location_df_from_selector(extracted_selector_list):
+    info_dict = {}
+    for i, (vaccine_info, location_info) in enumerate(zip(extracted_selector_list[::2],extracted_selector_list[1::2])):
+        info_dict[i] = dict()
+        info_dict[i]['vaccine_info'] = vaccine_info.text
+        info_dict[i]['location_info'] = location_info.text
+
+    vaccines_locations_df = pd.DataFrame.from_dict({index: {**(process_vaccine_info(vaccine_and_location)), **(process_location_info(vaccine_and_location))} for index, vaccine_and_location in info_dict.items()}, orient='index')
+
+    return vaccines_locations_df
+
+def cleaned_vaccines_location_df(df):
+    df['Early use in'] = df['Early use in'].str.rstrip(' .') #remove dots in the end
+    df['Limited use in'] = df['Limited use in'].str.rstrip(' .').str.replace('NEW', '') #remove dots in the end and NEW
+    df['Emergency use in'] = df['Emergency use in'].str.replace('NEW', '').str.replace("Emergency use validation from the World Health Organization",'').str.rstrip(' .') #remove dots in the end and NEW, "Emergency use validation from the World Health Organization."
+    df['Approved for use in'] = df['Approved for use in'].str.rstrip(' .').str.replace('NEW', '') #remove NEW and .
+    df = df .drop(columns=['None'])
+    return df
+
+def processed_vaccines_location_df(cleaned_df):
+    cleaned_df['Approved for use in'] = cleaned_df['Approved for use in'].apply(lambda x: list(map(str.strip, x.split(','))) if pd.notna(x) else x)
+    cleaned_df['Emergency use in'] = cleaned_df['Emergency use in'].apply(lambda x: list(map(str.strip, x.split(','))) if pd.notna(x) else x)
+    cleaned_df['Limited use in'] = cleaned_df['Limited use in'].apply(lambda x: list(map(str.strip, x.split(','))) if pd.notna(x) else x)
+    cleaned_df['Early use in'] = cleaned_df['Early use in'].apply(lambda x: list(map(str.strip, x.split(','))) if pd.notna(x) else x)
+    return cleaned_df
+
+def get_approved_vaccines():
+    r = get_NYT_webpage(NYT_URI)
+    extracted_info_list = r.html.find(SELECTOR_APPROVED)
+    vaccines_locations_df = get_vaccine_location_df_from_selector(extracted_info_list)
+    vaccines_locations_df = cleaned_vaccines_location_df(vaccines_locations_df)
+    vaccines_locations_df = processed_vaccines_location_df(vaccines_locations_df)
+    return vaccines_locations_df
+
+def save_approved_vaccines_nyt(df):
+    nyt_dataset_path = get_nyt_dataset_path('nyt_approved_vaccines.csv')
+    df.to_csv(nyt_dataset_path, index=False)
+
+def get_phase3_vaccines():
+    pass
+
+def get_phase2_vaccines():
+    pass
+
+def get_phase1_vaccines():
+    pass
+
+def get_preclinical_vaccines():
+    pass
+
+def get_abandoned_vaccines():
+    pass
+
+def get_all_vaccines():
+    pass
+
+if __name__=='__main__':
+    approved_vaccines_df = get_approved_vaccines()
+    save_approved_vaccines_nyt(approved_vaccines_df)
